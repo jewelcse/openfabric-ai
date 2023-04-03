04a75b908a5e23b132a712b7ee7436fb98214505
jewelcse <jewelcse045@gmail.com>
2023-04-03 09:34:03 +0600

    completed docker java client configuration

--- openfabric-api/src/main/java/ai/openfabric/api/config/MyDockerClientConfig.java
+++ openfabric-api/src/main/java/ai/openfabric/api/config/MyDockerClientConfig.java
@@ -1,18 +1,44 @@
 package ai.openfabric.api.config;
 
+import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Configuration
 public class MyDockerClientConfig {
 
+
     @Bean
     public DockerClientConfig dockerClientConfig() {
-        DefaultDockerClientConfig.Builder builder = DefaultDockerClientConfig.createDefaultConfigBuilder()
+        return DefaultDockerClientConfig.createDefaultConfigBuilder()
                 .withDockerHost("tcp://localhost:2375")
-                .withDockerTlsVerify(false);
-        return builder.build();
+                .withDockerTlsVerify(false)
+                .build();
     }
+
+    @Bean
+    public DockerHttpClient  dockerHttpClient() {
+        return new ApacheDockerHttpClient.Builder()
+                .dockerHost(dockerClientConfig().getDockerHost())
+                .sslConfig(dockerClientConfig().getSSLConfig())
+                .maxConnections(100)
+                .connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(45))
+                .build();
+
+    }
+
+    @Bean
+    public DockerClient dockerClient(){
+        return DockerClientImpl.getInstance(dockerClientConfig(),dockerHttpClient());
+    }
+
+
 }
